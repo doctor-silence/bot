926bd6462d0e59d2326187d767129269797e02c3
admin <admin@MacBook-Pro.local>
2022-08-09 13:08:19 +0300

    new_bot

--- telegram_bot/simple_bot.py
+++ telegram_bot/simple_bot.py
@@ -1,10 +1,40 @@
-from lib2to3.pgen2 import token
-import telebot
+import telebot, wikipedia, re
 
-bot_client = telebot.TeleBot(token='5477763774:AAHesdvLdwnzKAe0R0AW7T1j-ENoEUuo8xc')
+bot = telebot.TeleBot(token='...')
 
-@bot_client.massage_handler(commands=['start'])
-def echo(massage):
-    c = 1
+wikipedia.set_lang("ru")
 
-bot_client.polling()
+
+def getwiki(s):
+    try:
+        ny = wikipedia.page(s)
+        wikitext = ny.content[:1000]
+        wikimas = wikitext.split('.')
+        wikimas = wikimas[:-1]
+        wikitext2 = ''
+
+        for x in wikimas:
+            if not ('==' in x):
+                if (len((x.strip())) > 3):
+                    wikitext2 = wikitext2 + x + '.'
+            else:
+                break
+        wikitext2 = re.sub('\([^()]*\)', '', wikitext2)
+        wikitext2 = re.sub('\([^()]*\)', '', wikitext2)
+        wikitext2 = re.sub('\{[^\{\}]*\}', '', wikitext2)
+        return wikitext2
+    except Exception as e:
+        return 'В энциклопедии нет информации об этом'
+
+
+@bot.message_handler(commands=["start"])
+def start(m, res=False):
+    bot.send_message(m.chat.id, 'Отправьте мне любое слово, и я найду его значение на Wikipedia')
+
+
+@bot.message_handler(content_types=["text"])
+def handle_text(message):
+    bot.send_message(message.chat.id, getwiki(message.text))
+
+
+bot.polling(none_stop=True, interval=0)
